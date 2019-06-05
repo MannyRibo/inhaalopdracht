89146882dde1775126779c20cc0c9fa360dac6c2
Gino Marciano <gino.bolt@hva.nl>
2019-06-05 22:58:09 +0200

    push

---
+++ src/main/java/com/inhaalopdracht/inhaalopdracht/InhaalopdrachtApplication.java
@@ -0,0 +1,15 @@
+package com.inhaalopdracht.inhaalopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class InhaalopdrachtApplication {
+
+	public static void main(String[] args) {
+		run(InhaalopdrachtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/inhaalopdracht/inhaalopdracht/api/HeroRepository.java
@@ -0,0 +1,13 @@
+package com.inhaalopdracht.inhaalopdracht.api;
+
+import com.inhaalopdracht.inhaalopdracht.model.Hero;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+public interface HeroRepository extends CrudRepository<Hero, Long> {
+    @RestResource(path = "name", rel="name")
+    @Query("from Hero h where lower(h.name) like CONCAT('%', lower(:contains), '%')")
+    public Iterable<Hero> findByName(@Param("contains") String name);
+}
