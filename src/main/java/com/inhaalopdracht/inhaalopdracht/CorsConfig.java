78de991db0ee5638b49a12007a9b92d467ceffbd
Gino Marciano <gino.bolt@hva.nl>
2019-06-07 13:54:57 +0200

    Expose IDs en CorsFilter

---
+++ src/main/java/com/inhaalopdracht/inhaalopdracht/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.inhaalopdracht.inhaalopdracht;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootConfiguration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList(
+                new String[] {"OPTIONS", "GET", "POST", "PUT", "DELETE"}));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
---
+++ src/main/java/com/inhaalopdracht/inhaalopdracht/api/RepositoryConfig.java
@@ -0,0 +1,15 @@
+package com.inhaalopdracht.inhaalopdracht.api;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import com.inhaalopdracht.inhaalopdracht.model.Hero;
+
+@SpringBootConfiguration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Hero.class);
+    }
+}
